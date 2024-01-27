72f36c142213c7949b860c8126298099535b1d3e
samsathish14 <141956679+samsathish14@users.noreply.github.com>
2024-01-27 16:10:44 +0400

    new to

---
+++ Assets/Scripts/Living.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Living : MonoBehaviour
+{
+    public int  maxHealth;
+    public int currentHealth;
+
+}
--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementScript : MonoBehaviour
+public class MovementScript : Living
 {
 
     public Rigidbody2D myBody;
